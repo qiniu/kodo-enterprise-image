894e61189f8fa463e424e2c134a4534570b51d87
haohaiwei <haohaiwei@haohaiweideMacBook-Pro.local>
2019-04-28 11:20:27 +0800

    添加licenes

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 
 if [ "$(hostname)" == "node1" ]
 then
-  tar -xf /qiniu/node1.tgz -C /home/qboxserver && chown -R qboxserver. /home/qboxserver && tar -xf /qiniu/node1rs.tgz -C /rs1 && chown -R qboxserver. /rs1 && tar -xf /qiniu/ng01.tgz -C /usr/local/openresty/nginx/conf.d/ && tar -xf /qiniu/sup01.tgz -C /etc/supervisor/conf.d/ && rm -rf /etc/chrony.conf && mv /qiniu/chrony.conf /etc/ && /usr/sbin/chronyd -f /etc/chrony.conf && rm -rf /qiniu && supervisord && /etc/init.d/openresty start
+  tar -xf /qiniu/node1.tgz -C /home/qboxserver && chown -R qboxserver. /home/qboxserver && tar -xf /qiniu/node1rs.tgz -C /rs1 && chown -R qboxserver. /rs1 && tar -xf /qiniu/ng01.tgz -C /usr/local/openresty/nginx/conf.d/ && tar -xf /qiniu/sup01.tgz -C /etc/supervisor/conf.d/ && /usr/sbin/chronyd -f /etc/chrony.conf && rm -rf /qiniu && supervisord && /etc/init.d/openresty start
   /bin/bash
 elif [ "$(hostname)" == "node2" ]
 then
